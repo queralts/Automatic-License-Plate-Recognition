86c7a01a250164842b1c9f679e81d340f19a667d
queralts <quersalv@gmail.com>
2025-09-29 13:41:18 +0200

    added visualization of lateral images

--- CharacterSegmentation/license_plate/LicensePlateCharacterSegmentation.py
+++ CharacterSegmentation/license_plate/LicensePlateCharacterSegmentation.py
@@ -85,22 +85,95 @@ def detectCharacterCandidates(image, reg, SHOW=0):
     return plate, thresh, MycharCandidates
 
 
+def detectCharacterCandidates_LoG(image, reg, SHOW=0):
+    # apply a 4-point transform to extract the license plate
+    plate = perspective.four_point_transform(image, reg)
+    plate = imutils.resize(plate, width=400)
+
+    if (SHOW):
+        cv2.imshow("Perspective Transform", plate)
+    
+    # extract the Value component from the HSV color space and apply adaptive thresholding
+    # to reveal the characters on the license plate
+    V = cv2.split(cv2.cvtColor(plate, cv2.COLOR_BGR2HSV))[2]        
+    thresh = cv2.adaptiveThreshold(V, 255 ,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 51, 15)
+    if (SHOW):
+        cv2.imshow("Adaptative Threshold", thresh)
+
+    # Structuring Element  rectangular shape (width 1 hight 3)  s
+    rectKernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1, 3))
+
+    # clossing follow by an opening to fill holes and join regions
+    thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, rectKernel,iterations = 2)
+    thresh = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, rectKernel,iterations = 2)
+
+    # resize the license plate region 
+    thresh = imutils.resize(thresh, width=400)
+
+    if (SHOW):
+        print("START DIMENSIONAL ANALYSIS")
+    
+    DimY, DimX = thresh.shape[:2]
+    (cnts,_) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # loop over the contours
+    MycharCandidates = np.zeros(thresh.shape, dtype="uint8")
+    for c in cnts:
+        # grab the bounding box associated with the contour and compute the area and
+        # aspect ratio
+        (x,y,w, h) = cv2.boundingRect(c)
+
+        # condition of not touching the border of the region
+        NotouchBorder = x!=0 and y!=0 and x+w!=DimX and y+h!=DimY
+        
+        if (NotouchBorder):
+            
+
+            # hight ratio of the blob numbers are blobs with a hight near the DimY
+            hW=(h / float(DimY))
+            area = cv2.contourArea(c)
+            if (SHOW):
+                print("AREA: ",area," ASPECT: ",hW)
+            heightRatio = 0.5 < hW <0.9
+            if (area>300) and (heightRatio):
+                
+                hull = cv2.convexHull(c)
+                cv2.drawContours(MycharCandidates, [hull], -1, 255, -1)
+    if (SHOW):            
+        print("END DIMENSIONAL ANALYSIS")
+
+    
+
+    # return the license plate region object containing the license plate, the thresholded
+    # license plate, and the character candidates
+    return plate, thresh, MycharCandidates
+
 if __name__ == "__main__":
 
     # Get the directory where the script is located
     script_dir = os.path.dirname(os.path.abspath(__file__))
     # Get file PlateRegions.npz
-    f = os.path.join(script_dir, "../../cropped_real_plates/Frontal/PlateRegions.npz")
+    file_frontal = os.path.join(script_dir, "../../cropped_real_plates/Frontal/PlateRegions.npz")
+    file_lateral = os.path.join(script_dir, "../../cropped_real_plates/Lateral/PlateRegions.npz")
 
-    data_frontal=np.load(f,allow_pickle=True) 
+    data_frontal = np.load(file_frontal,allow_pickle=True) 
+    data_lateral = np.load(file_lateral, allow_pickle=True)
+
     #use data.files to get data variables names 
+
+    # Get data from frontal images
     regionsImCropped_frontal = data_frontal['regionsImCropped']
     regionsIm_frontal = data_frontal['regionsIm']
     imageIDs_frontal = data_frontal['imID']
 
-    # Apply pipeline on all frontal cropped images 
-    for img, reg in zip(imageIDs_frontal, regionsImCropped_frontal):
-        image_path = os.path.join(script_dir, "../../cropped_real_plates/Frontal", img+"_MLPlate0.png")
+    # Get data from lateral images
+    regionsImCropped_lateral = data_lateral['regionsImCropped']
+    regionsIm_lateral = data_lateral['regionsIm']
+    imageIDs_lateral = data_lateral['imID']
+
+    # Apply pipeline on all lateral cropped images 
+    for img, reg in zip(imageIDs_lateral, regionsImCropped_lateral):
+        image_path = os.path.join(script_dir, "../../cropped_real_plates/Lateral", img+"_MLPlate0.png")
         image = cv2.imread(image_path)
 
         # Use character detection pipeline on that image
--- CharacterSegmentation/license_plate/tempCodeRunnerFile.py
+++
@@ -1,1 +0,0 @@
-    print("Reg points:\n", reg)
